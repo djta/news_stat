09e9824a9cf3cf172dc8f8d93a752e0da9290054
383817938@qq.com <thankyour123>
2018-03-29 02:06:12 +0800

    ADD:1.2.3 新增bidcontext

--- src/main/java/quant/backtest/BackTestContext.java
+++ src/main/java/quant/backtest/BackTestContext.java
@@ -2,6 +2,8 @@ package quant.backtest;
 
 import domain.MarketDomain;
 import jdbc.impl.MarketDaoImpl;
+import quant.constant.TendencySign;
+import quant.tendencyStat.TendencyContext;
 
 import java.util.List;
 
@@ -15,8 +17,49 @@ public class BackTestContext {
         System.out.println("ok");
         MarketDaoImpl marketDao = new MarketDaoImpl();
         List<MarketDomain> marketDomains = marketDao.getKlineData("btcusdt");
-        for (MarketDomain md : marketDomains) {
-            System.out.println(md);
+//        for (MarketDomain md : marketDomains) {
+//            System.out.println(md);
+//        }
+        BidContext bc = new BidContext(10000);
+        for (int i = 0; i < marketDomains.size() - 50; i++) {
+            List<MarketDomain> list = marketDomains.subList(i, i + 50);
+            int result = predictTendency(list);
+            if (result >= 3) {
+                System.out.println(list.get(list.size() - 1));
+                double colse = list.get(list.size() - 1).getClose();
+                System.out.println("买入：" + colse);
+                bc.buy(colse);
+            } else if (result <= -3) {
+                double close = list.get(list.size() - 1).getClose();
+                bc.sell(close);
+                System.out.println(list.get(list.size() - 1));
+                System.out.println("卖出：" + close);
+            }
         }
+        bc.sell(marketDomains.get(marketDomains.size() - 1).getClose());
+        System.out.println("result:" + bc);
+
+
+    }
+
+    public static void predict(List<MarketDomain> marketDomains) {
+        TendencySign ts = TendencyContext.dmaSign(marketDomains, 2, 20, 5);
+        TendencySign ts1 = TendencyContext.macdSign(marketDomains, 2, 20, 5);
+        TendencySign ts2 = TendencyContext.maSign(marketDomains, 2, 20);
+        TendencySign ts3 = TendencyContext.trixSign(marketDomains, 2, 20);
+        if (ts.value + ts1.value + ts2.value + ts3.value >= 3) {
+            System.out.println("bull");
+        } else if (ts.value + ts1.value + ts2.value + ts3.value <= -3) {
+            System.out.println("Bear");
+        } else {
+            System.out.println("Hold");
+        }
+    }
+
+    public static int predictTendency(List<MarketDomain> marketDomains) {
+        int result = TendencyContext.dmaSign(marketDomains, 2, 20, 5).value
+                + TendencyContext.macdSign(marketDomains, 2, 20, 5).value
+                + TendencyContext.maSign(marketDomains, 2, 20).value + TendencyContext.trixSign(marketDomains, 2, 20).value;
+        return result;
     }
 }
---
+++ src/main/java/quant/backtest/BidContext.java
@@ -0,0 +1,65 @@
+package quant.backtest;
+
+/**
+ * Created by 范志伟 on 2018-03-29.
+ */
+public class BidContext {
+    private double amount;//拥有币量
+    private double fund;//初始化资金
+    private double rate = 0.002;//费率；
+    private double cost = 0;//手续费
+    private int buy = 0;//买次数；
+    private int sell = 0;//卖次数；
+
+    public BidContext(double fund) {
+        this.fund = fund;
+    }
+
+    public static void main(String args[]) {
+
+    }
+
+    synchronized public boolean buy(double close) {
+        if (fund <= 0) {
+            return false;
+        }
+        cost += fund * rate;
+        fund -= fund * rate;
+        amount = fund / close;
+        buy++;
+        fund = 0;
+        System.out.println("buy success");
+        return true;
+
+    }
+
+    public boolean sell(double close) {
+        if (amount <= 0) {
+            return false;
+        }
+        cost += amount * rate * close;
+        amount -= amount * rate;
+        fund = close * amount;
+        System.out.println("sell success:"+fund);
+        amount = 0;
+        sell++;
+        System.out.println("sell success");
+        return true;
+    }
+
+    public double profit() {
+        return fund;
+    }
+
+    @Override
+    public String toString() {
+        return "BidContext{" +
+                "amount=" + amount +
+                ", fund=" + fund +
+                ", rate=" + rate +
+                ", cost=" + cost +
+                ", buy=" + buy +
+                ", sell=" + sell +
+                '}';
+    }
+}
