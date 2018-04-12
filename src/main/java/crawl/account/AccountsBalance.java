c76b06cae25014c1ab879fdd0191373b58a01bdc
383817938@qq.com <thankyour123>
2018-04-13 02:56:44 +0800

    ADD:1.2.6.9

--- src/main/java/crawl/account/AccountsBalance.java
+++ src/main/java/crawl/account/AccountsBalance.java
@@ -2,10 +2,13 @@ package crawl.account;
 
 import com.alibaba.fastjson.JSON;
 import crawl.account.domain.AccountsBalanceDomain;
+import crawl.account.domain.BalanceSymbolDetail;
 import util.Constants;
 import util.HmacSHA256;
 import util.HttpUtil;
 import util.StringUtil;
+
+import java.util.List;
 
 /**
  * Created by 范志伟 on 2018-04-12.
@@ -21,7 +24,10 @@ public class AccountsBalance {
 //        String url = Constants.URL_ACCOUNT_BALANCE + rawStr;
 //        System.out.println(url);
         System.out.println(getAccountBalanceInfo());
+        BalanceSymbolDetail bsd = getSymbolBalanceInfo("zil");
+        System.out.println(bsd);
     }
+
 
     private static String getSignature(String path) {
         String param = StringUtil.accountparmsAsciiSort();
@@ -34,8 +40,23 @@ public class AccountsBalance {
         String rawStr = "?" + StringUtil.accountparmsAsciiSort() + "&Signature=" + signature;
         String url = Constants.URL_ACCOUNT_BALANCE + rawStr;
         String urlData = HttpUtil.doGetData(url);
-        AccountsBalanceDomain mmd = JSON.parseObject(urlData, AccountsBalanceDomain.class);
-        return mmd;
+        AccountsBalanceDomain accountsBalanceDomain = JSON.parseObject(urlData, AccountsBalanceDomain.class);
+        return accountsBalanceDomain;
+    }
+
+    public static BalanceSymbolDetail getSymbolBalanceInfo(String symbol) {
+        AccountsBalanceDomain accountsBalanceDomain = getAccountBalanceInfo();
+        String status = accountsBalanceDomain.getStatus();
+        if (!status.equals("ok")) {
+            return null;
+        }
+        List<BalanceSymbolDetail> balanceSymbolDetails = accountsBalanceDomain.getData().getList();
+        for (BalanceSymbolDetail bd : balanceSymbolDetails) {
+            if (bd.getCurrency().equals(symbol)) {
+                return bd;
+            }
+        }
+        return null;
     }
 
 }
