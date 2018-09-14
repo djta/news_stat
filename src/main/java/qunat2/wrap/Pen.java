1d9cab0350afc5ef0b97fa5e59dc0bcae3e9c55c
383817938@qq.com <thankyour123>
2018-09-14 08:21:33 +0800

    ADD:1.3.1.1

--- src/main/java/qunat2/wrap/Pen.java
+++ src/main/java/qunat2/wrap/Pen.java
@@ -33,7 +33,7 @@ public class Pen {
                     && Math.abs((bottomCount - topCount)) > 1
                     && (topDomain.getHigh() > bottomDomain.getHigh()
                     && topDomain.getLow() > bottomDomain.getLow())
-            ) {
+                    ) {
                 if ((topCount < bottomCount && penDomains.size() == 0)
                         || (penDomains.size() > 0 && penDomains.get(penDomains.size() - 1).getPartEnum() == PartEnum.BOTTOM
                 )) {//up
@@ -60,48 +60,49 @@ public class Pen {
         int bottomCount = 0;
         List<PartDomain> penDomains = new ArrayList<PartDomain>();
         PartEnum handleEnum = null;
-        PartDomain handleDomain = null;
-        int start = 0;
-        int end = 0;
+        int count1 = 0;
+        int count2 = 0;
+        PartDomain partDomain1 = null;
+        PartDomain partDomain2 = null;
         for (int i = 0; i < partDomainList.size(); i++) {
             PartDomain partDomain = partDomainList.get(i);
 
             if (partDomain.getPartEnum() != PartEnum.LEVEL) {
-                if (penDomains.size() == 0) {//start run 1 times
-                    handleDomain = partDomain;
-                    penDomains.add(partDomain);
-                    start = i;
+                if (partDomain1 == null) {//start run 1 times
+                    partDomain1 = partDomain;
+                    count1 = i;
                     continue;
                 }
 
-                if (handleDomain != null && handleDomain.getPartEnum() != partDomain.getPartEnum()) {
-                    handleDomain = partDomain;
-                    end = i;
-                } else if (handleDomain != null && handleDomain.getPartEnum() == partDomain.getPartEnum()) {//和原来的重复
-                    if ((handleDomain.getPartEnum() == PartEnum.BOTTOM &&
-                            handleDomain.getLow() > partDomain.getLow())
-                            || (handleDomain.getPartEnum() == PartEnum.TOP &&
-                            handleDomain.getHigh() < partDomain.getHigh())) {
-                        penDomains.remove(penDomains.size() - 1);
-                        handleDomain = partDomain;
-                        penDomains.add(handleDomain);
+                if (partDomain1.getPartEnum() != partDomain.getPartEnum()) {//不重复
+                    if (Math.abs(count1 - i) > 1) {//匹配成功
+                        penDomains.add(partDomain1);
+                        partDomain1 = null;
                     }
-                    continue;
+                } else if (partDomain1.getPartEnum() == partDomain.getPartEnum()) {//和原来的重复
+//                    if ((partDomain1.getPartEnum() == PartEnum.BOTTOM &&
+//                            partDomain1.getLow() > partDomain.getLow())
+//                            || (partDomain1.getPartEnum() == PartEnum.TOP &&
+//                            partDomain1.getHigh() < partDomain.getHigh())) {
+                    penDomains.remove(penDomains.size() - 1);
+                    partDomain1 = partDomain;
+                    penDomains.add(partDomain1);
                 }
-                if (handleEnum != null && handleEnum == partDomain.getPartEnum()) {
+                continue;
+            }
+            if (handleEnum != null && handleEnum == partDomain.getPartEnum()) {
 
-                }
-
-
-                if (handleDomain != null && Math.abs(end - start) > 1) {//写入
-                    penDomains.add(partDomain);
-                    handleEnum = partDomain.getPartEnum();
-                    handleDomain = null;
-                }
             }
 
 
+            if (partDomain1 != null && Math.abs(count1 - count2) > 1) {//写入
+                penDomains.add(partDomain);
+                handleEnum = partDomain.getPartEnum();
+                partDomain1 = null;
+            }
         }
+
+
         return penDomains;
     }
 
